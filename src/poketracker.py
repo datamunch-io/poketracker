5555acb041265c5861a5c9caee361ff2dea33a85
Michael Rodriguez <mrodriguez@datamunch.io>
2024-05-23 20:42:04 -0400

    poketracker

---
+++ src/poketracker.py
@@ -0,0 +1,25 @@
+from google.cloud import bigquery 
+
+from src.constants import constants 
+
+
+class Poketracker:
+    def __init__(self):
+        self.client = self.initialize_bq_client()
+        self.searchables = self.get_searchables()
+        
+    def get_searchables(self):
+        # TODO: Create a cards table to separate the searchables and card prices.
+        query = f"""
+            SELECT card_id, CONCAT(card_name, '-', card_set_code, ' ', card_print_number,  '/', set_printed_total) AS searchable
+            FROM {constants.DATASET_ID}.{constants.TABLES.get('prices')}
+        """
+        
+        job = self.client.query(query=query)
+        results = job.result()
+        return [{row.searchable: row.card_id} for row in results]
+        
+    @staticmethod 
+    def initialize_bq_client():
+        return bigquery.Client(project=constants.PROJECT_NAME)
+        
